013e224614b55abc345bfeb6c54156c8d97bb4d5
muhammedbayhan <76851132+muhammedbayhan@users.noreply.github.com>
2023-11-12 16:29:17 +0300

    categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import Header from "./components/Header/Header"
+import Categories from "./components/categories/Categories"
 
 
 function App() {
 
   return (
-    <div>
+    <>
     <Header/>
+    <div className="home px-6 flex justify-between gap-10">
+      <div className="categories pb-64 flex-1 overflow-auto max-h-[calc(100vh-_-112px)]">
+        <Categories/>
+      </div>
+      <div className="products flex-[8]">
+        <div>products</div>
+      </div>
+      <div className="cartTotals">
+        <div>cardTotals</div>
+      </div>
+
     </div>
+    </>
   )
 }
 
---
+++ frontend/src/components/categories/Categories.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <ul className='flex gap-4 flex-col text-xl '>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Tümü</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Yiyecek</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>İçecek</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Teknoloji</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Sağlık</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Spor</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Eğitim</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Ev ve Bahçe</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Kitaplar ve Edebiyat</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Doğa ve Çevre</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Hobi ve El Sanatları</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Hayvanlar</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Otomotiv</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Eğitim</span>
+</li>
+<li className='bg-slate-500 px-6 py-6 text-white cursor-pointer hover:bg-slate-700 transition-all text-center'>
+    <span>Eğlence</span>
+</li>
+
+
+    </ul>
+  )
+}
+
+export default Categories
